1adecb68983a9cc811af3d9e483e2b24c5bb5203
Anson Chan <ansonchan100@gmail.com>
2022-05-20 16:52:33 +0100

    Terraform updates

---
+++ terraform-guide.md
@@ -0,0 +1,25 @@
+
+`terraform init`
+Set up terraform environment on localhost
+
+`terraform plan`
+Make terraform aware of our action plan
+
+`terraform apply`
+Apply the action plan 
+
+`terraform destroy`
+terminating the instance
+
+Within `main.tf`  
+    var `provider "<provider>"`: we can specify our cloud provider, e.g.
+        `region`
+
+    `resource`: specify the resource we want to create
+
+
+Short summary of Terraform: https://www.youtube.com/watch?v=tomUWcQ0P3k
+
+Longer summary of Terraform: https://www.youtube.com/watch?v=l5k1ai_GBDE&t=6s
+
+Long tutorial on Terraform: https://www.youtube.com/watch?v=SLB_c_ayRMo
---
+++ terraform/main.tf
@@ -0,0 +1,101 @@
+# GOAL: create a script to initialise terraform to download dependencies required for AWS
+
+# 1. Create a script to launch ec2
+
+# first step to create block of code to communicate with aws
+provider "aws" {
+    region = var.aws_region
+}
+
+# let terraform know which service/resource we want to create
+resource "aws_vpc" "terraform_vpc" {
+    cidr_block = var.vpc_cidr
+    instance_tenancy = "default"
+    enable_dns_hostnames = true
+
+    tags = {
+      "Name" = "${var.name}_terraform_vpc"
+    }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  tags = {
+    Name = "${var.name}_terraform_igw"
+  }
+}
+
+resource "aws_subnet" "terraform_subnet" {
+    vpc_id = aws_vpc.terraform_vpc.id
+    cidr_block = var.public_subnet_cidr  
+    depends_on = [aws_internet_gateway.gw]
+
+    tags = {
+      "Name" = "${var.name}_terraform_subnet"
+    }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block = var.route_1
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.name}_terraform_route_table"
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+    subnet_id = aws_subnet.terraform_subnet.id
+    route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "terraform_sg" {
+    vpc_id = aws_vpc.terraform_vpc.id
+
+    egress = var.sg_outbound_rules
+        
+    ingress = var.sg_inbound_rules
+
+    tags = {
+      "Name" = "${var.name}_terraform_sg"
+    }
+}
+
+resource "aws_instance" "app_instance" {
+
+    subnet_id = aws_subnet.terraform_subnet.id
+    # depends_on = [aws_internet_gateway.gw]
+
+    # choose an AMI to create ec2
+    ami = var.node_ami_id
+
+    # what type of instance to launch
+    instance_type = var.aws_instance_type
+
+    # do we want to have it globally available - public ip
+    associate_public_ip_address = true
+
+    security_groups = ["${aws_security_group.terraform_sg.id}"]
+
+    # name your instance 
+    tags = {
+        Name = "${var.name}-terraform-app"
+    }
+
+    # attach the file.pem for access from localhost
+    key_name = var.aws_key_name
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file(var.aws_key_path)
+      timeout     = "2m"
+   }
+}
+
+# # 2. Move the app folder to the instance
