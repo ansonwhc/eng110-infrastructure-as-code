24fa41460c929dc5d28f65d3c0f87ccf2df86b8a
Anson Chan <ansonchan100@gmail.com>
2022-06-30 14:19:58 +0100

    Updated info to match repo setup demo

--- ansible-demo-project/vagrant-provision/controller-provisioning.sh
+++ ansible-demo-project/vagrant-provision/controller-provisioning.sh
@@ -16,7 +16,7 @@ sudo apt-get install python3-pip -y
 # - https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html (accessed 29/06/2022)
 # - https://docs.ansible.com/ansible/latest/reference_appendices/python_3_support.html (accessed 29/06/2022)
 pip3 install ansible
-sudo echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc  # for warnings
+sudo echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 sudo echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.profile
 source ~/.profile
